45e41255351bdbe969ba3cd19833e6eb71d428c8
App Academy Student <student@appacademy.io>
2014-02-17 19:39:13 -0800

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = 'afc18742c0e0994a7eb54229da10d6720c1480c163c4f07be1929a4a4aa4924075ecb92c3d282ddbfed0993c1aa5f9c2a2ec113ca8889b41e2d08b36075a5b1f'
